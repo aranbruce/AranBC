28ef0f1d780f87db9860d8bb1c1aedabe4c46c47
Aran <aranbruce@gmail.com>
2017-12-11 21:59:32 +0000

    sw update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js', {
+    navigator.serviceWorker.register('/sw.js', {
         scope: '/'
     }).then(function() {
         // success
@@ -11,12 +11,12 @@ if ('serviceWorker' in navigator) {
 var CACHE_NAME = 'cache-v1';
 
 var resourcesToCache = [
-  './',
-  './css/styles.css',
-  './scripts/jsScripts.js',
-  './index.html',
-  './manifest.json',
-  './favicon.png'
+  '/',
+  '/css/styles.css',
+  '/scripts/jsScripts.js',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.png'
 ];
 
 self.addEventListener('install', function(event) {
@@ -26,22 +26,6 @@ self.addEventListener('install', function(event) {
       .then(function(cache) {
         // add all app assets to the cache
         return cache.addAll(resourcesToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // try to find corresponding response in the cache
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          // cache hit: return cached result
-          return response;
-        }
-
-        // not found: fetch resource from the server
-        return fetch(event.request);
       })
   );
 });
