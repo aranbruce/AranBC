05467497b1f117468b160cba61528e91dc165331
Aran <aranbruce@gmail.com>
2017-12-12 20:22:43 +0000

    sw update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'v1';
+var cacheName = 'v2';
 var cacheFiles = [
   'index.html',
   'favicon.png',
@@ -12,7 +12,6 @@ var cacheFiles = [
 
 self.addEventListener('install', function(e) {
   console.log("[ServiceWorker] Installed")
-
   e.waitUntil(
     caches.open(cacheName).then(function(cache){
       console.log("[ServiceWorker] Caching cacheFiles");
@@ -23,6 +22,16 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   console.log("[ServiceWorker] Activated")
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(thisCacheName) {
+        if (thisCacheName !== cacheName) {
+          console.log("[ServiceWorker] Removing Cached Files from ", thisCacheName);
+          return caches.delete(thisCacheName);
+        }
+      }))
+    })
+  )
 })
 
 self.addEventListener('fetch', function(e) {
