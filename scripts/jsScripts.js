4b469580f900d6f8da7ad6e73648e575c9ad6f74
Aran <aranbruce@gmail.com>
2017-12-20 23:26:04 +0000

    updates to scripts

--- scripts/jsScripts.js
+++ scripts/jsScripts.js
@@ -3,52 +3,52 @@
 
 function openCloseMenu() {
 
-  /*
-  //hide mobile menu when it is closed
-  document.getElementById("menu").classList.toggle("hide");
-  */
-  //show mobile menu when it is open
-  document.getElementById("menu").classList.toggle("show");
+/*
+//hide mobile menu when it is closed
+document.getElementById("menu").classList.toggle("hide");
+*/
+//show mobile menu when it is open
+document.getElementById("menu").classList.toggle("show");
 
-  //Change burger to 'x'
-  document.getElementById("burger").classList.toggle("burgerContainerClose");
+//Change burger to 'x'
+document.getElementById("burger").classList.toggle("burgerContainerClose");
 
-  /*
+/*
 
-  //hide elemensts when burger is open
-    var hiddenOnOpenMenu = document.getElementsByClassName("hiddenOnOpenMenu")
-    for (var i = 0; i < hiddenOnOpenMenu.length; i++) {
-      hiddenOnOpenMenu[i].classList.toggle("hide")
-      }
+//hide elemensts when burger is open
+  var hiddenOnOpenMenu = document.getElementsByClassName("hiddenOnOpenMenu")
+  for (var i = 0; i < hiddenOnOpenMenu.length; i++) {
+    hiddenOnOpenMenu[i].classList.toggle("hide")
+    }
 
-    //Change background when mobile menu is open
-    document.getElementById("bodyBackground").classList.toggle("menuOpen");
+  //Change background when mobile menu is open
+  document.getElementById("bodyBackground").classList.toggle("menuOpen");
 
-  */
+*/
 
-  }
+}
 
-  //Smooth Scrolling for links
-    $(document).ready(function(){
-      // Add smooth scrolling to all links
-      $("a").on('click', function(event) {
+//Smooth Scrolling for links
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-          // Store hash
-          var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-          });
-        } // End if
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
       });
-    });
+    } // End if
+  });
+});
