7dd45f02b766d90cbb877cb58c34a2bb3d8425a8
Aran <aranbruce@gmail.com>
2018-01-27 17:21:05 +0000

    remove redundant js fade

--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -882,11 +882,3 @@ sr.reveal('.bubble', {
   delay: 800,
   duration: 1000
  }, 100);
-
- sr.reveal('.posterImage', {
-   duration: 800,
-   origin: 'bottom',
-   scale: 1,
-   distance: '5px',
-   delay: 100
- })
