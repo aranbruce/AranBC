23f3f3265c3a69352e650c4263f5240ba504d547
Aran <34005105+aranbruce@users.noreply.github.com>
2018-01-30 18:31:04 +0000

    sw update

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
+  '/',
+  'index.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
