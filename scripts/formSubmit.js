310a4a9078fda8969f68133585d8a14496db9d1b
Aran <aranbruce@gmail.com>
2018-02-10 23:47:54 +0000

    e changed to event

--- scripts/formSubmit.js
+++ scripts/formSubmit.js
@@ -1,8 +1,8 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxtiYIHGmdJqPYQMZ5LhSjsgr6kuiwscgr4_DCgbPfkclgKamUJ/exec'
 const form = document.forms['enquiryForm']
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+form.addEventListener('submit', event => {
+  event.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => console.log('Success!', response))
     .catch(error => console.error('Error!', error.message))
