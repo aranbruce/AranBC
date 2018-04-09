ce472f33b93a85ad31118f634e4e8146410e9a46
Aran <aranbruce@gmail.com>
2018-04-09 23:29:22 +0100

    removed redundant node modules

--- src/scripts/openCloseMenu.js
+++ src/scripts/openCloseMenu.js
@@ -1,19 +1,17 @@
 let $ = require('jquery');
 
 window.onload = function() {
-  if(menu != null) {
-    var menu = document.getElementById("menu")
-  }
+  var menu = document.getElementById("menu")
   var burger = document.getElementById("burger")
   var navItems = document.getElementsByClassName("navItem")
 
   function openCloseMenu(){
-    if(menu != null) {
+    if(window.document.URL.indexOf("citysnapp") >= 0) {
+      window.location.href='/#projects'
+    }
+    else {
       menu.classList.toggle("show");
       burger.classList.toggle("burgerContainerClose");
-    }
-    else if (window.location.href='/citysnapp') {
-      window.location.href='/#projects'
     }
   }
 
