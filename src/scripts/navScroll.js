5bd5651cce11bbd6be73e0ac9ad314ed3d41b4d9
Aran <aranbruce@gmail.com>
2019-02-07 21:32:25 +0000

    Added work and skills sections

--- src/scripts/navScroll.js
+++ src/scripts/navScroll.js
@@ -1,9 +1,16 @@
 let scrollpos = window.scrollY
 const nav = document.querySelector("nav")
+const burger = document.getElementById("burger")
 const nav_height = nav.offsetHeight
 
-const add_class_on_scroll = () => nav.classList.add("nav--scroll")
-const remove_class_on_scroll = () => nav.classList.remove("nav--scroll")
+const add_class_on_scroll = () => {
+    nav.classList.add("nav--scroll")
+    burger.classList.add("burger-container--scroll")
+}
+const remove_class_on_scroll = () => {
+    nav.classList.remove("nav--scroll")
+    burger.classList.remove("burger-container--scroll")
+}
 
 window.addEventListener('scroll', function() { 
     scrollpos = window.scrollY;
