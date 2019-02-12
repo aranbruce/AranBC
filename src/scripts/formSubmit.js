22f4a999e9af96fa5c75e8bd7b6564296fcbe60a
Aran <aranbruce@gmail.com>
2019-02-12 21:32:33 +0000

    fixed form submit

--- src/scripts/formSubmit.js
+++ src/scripts/formSubmit.js
@@ -1,17 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxtiYIHGmdJqPYQMZ5LhSjsgr6kuiwscgr4_DCgbPfkclgKamUJ/exec'
-const form = document.getElementById('enquiryForm');
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwcGSl46eEqo95KJ5_PpeVrFzLurzwG8IzI5VuQmIhmHzU7gyI/exec'
+const form = document.getElementById('enquiryForm')
 
-var formElement = document.getElementById("enquiryForm");
-if(formElement != null) {
-  form.addEventListener('submit', event => {
-    event.preventDefault()
+if(form != null) {
+  form.addEventListener('submit', e => {
+    e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
-      setTimeout(thankYouRedirect, 500);
+      setTimeout(thankYouRedirect, 4000);
       function thankYouRedirect() {
         if (response = true) {
-          location.href = "/thankyou";
+          location.href = "/thankyou.html";
         }
       }
   })
