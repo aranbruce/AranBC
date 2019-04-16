85fa77c49277d38ce6be1110df383225594a323b
Aran <aranbruce@gmail.com>
2019-04-16 21:30:06 +0100

    added form labels for accesibility and changed button text on form submit

--- src/scripts/formSubmit.js
+++ src/scripts/formSubmit.js
@@ -1,17 +1,19 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwcGSl46eEqo95KJ5_PpeVrFzLurzwG8IzI5VuQmIhmHzU7gyI/exec'
 const form = document.getElementById('enquiryForm')
+const formButton = document.getElementById("formButton");
 
 if(form != null) {
   form.addEventListener('submit', e => {
     e.preventDefault()
+    e.stopImmediatePropagation()
+    formButton.innerHTML = formButton.innerHTML.replace('Send', "Sending");
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
-      setTimeout(thankYouRedirect, 4000);
-      function thankYouRedirect() {
-        if (response = true) {
-          location.href = "/thankyou.html";
-        }
-      }
+      .then(function thankYouRedirect() {
+          if (response = true) {
+            location.href = "/thankyou";
+          }
+        })
   })
 }
