25242e92e12a6ff08345ab999ce9611ca04152c2
Aran <aranbruce@gmail.com>
2019-06-08 17:24:22 +0100

    updated package dependencies

--- src/scripts/formSubmit.js
+++ src/scripts/formSubmit.js
@@ -1,19 +1,20 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwcGSl46eEqo95KJ5_PpeVrFzLurzwG8IzI5VuQmIhmHzU7gyI/exec'
-const form = document.getElementById('enquiryForm')
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwcGSl46eEqo95KJ5_PpeVrFzLurzwG8IzI5VuQmIhmHzU7gyI/exec";
+const form = document.getElementById("enquiryForm");
 const formButton = document.getElementById("formButton");
 
-if(form != null) {
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    e.stopImmediatePropagation()
-    formButton.innerHTML = formButton.innerHTML.replace('Send', "Sending");
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
+if (form != null) {
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    formButton.innerHTML = formButton.innerHTML.replace("Send", "Sending");
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then(response => console.log("Success!", response))
+      .catch(error => console.error("Error!", error.message))
       .then(function thankYouRedirect() {
-          if (response = true) {
-            location.href = "/thankyou";
-          }
-        })
-  })
+        if ((response = true)) {
+          location.href = "/thankyou";
+        }
+      });
+  });
 }
